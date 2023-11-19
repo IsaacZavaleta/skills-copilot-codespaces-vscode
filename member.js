2030b2c629155d2b2016290eb7e9747c93dda743
IsaacZavaleta <106347513+IsaacZavaleta@users.noreply.github.com>
2023-11-19 00:31:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    return {
+        restrict: 'E',
+        templateURL: 'modules/skills/views/member.html',
+        controller: 'SkillMemberController',
+        controllerAs: 'vm',
+        scope: {
+            member: '='
+        }
+    };
+}
